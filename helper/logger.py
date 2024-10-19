6b822e1f16ff46553a34004c8971579d58ace11d
Alok <alokrkmv12@gmail.com>
2024-10-19 02:00:59 -0400

    Added helper functions for reading lookup and port data

---
+++ helper/logger.py
@@ -0,0 +1,17 @@
+import logging
+
+class Logger:
+    def __init__(self):
+        # Create a logger
+        self.logger = logging.getLogger(__name__)
+
+        # Set the logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+        self.logger.setLevel(logging.DEBUG)
+
+        # Create a handler for output (e.g., console)
+        self.handler = logging.StreamHandler()
+
+        # Add the handler to the logger
+        self.logger.addHandler(self.handler)
+    def get_logger(self):
+        return self.logger
